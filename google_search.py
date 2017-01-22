6546527b9917508d37b52cee6d7071b92c29b417
Aleks Kivuls <akivuls@yahoo.com>
2017-01-21 22:02:36 -0800

    Google search browser open

---
+++ google_search.py
@@ -0,0 +1,24 @@
+import webbrowser
+import re
+
+def launch_google(query):
+
+    address = "https://www.google.com/#q="
+    query_words = query.split()
+
+    # REMOVE NONALPHANUMERIC CHARS
+    query = re.sub(r'\W+', ' ', query)
+    
+    # ENSURE LOWER CASE
+    query = query.lower()
+
+    # REMOVE LEADING AND TRAILING WHITE SPACES
+    query = query.strip()
+
+    # REPLACE WHITE SPACES WITH '+'
+    query = query.replace(" ", "+")
+
+    # APPEND QUERY TO ADDRESS
+    url = address + query
+    
+    webbrowser.open_new(url)
